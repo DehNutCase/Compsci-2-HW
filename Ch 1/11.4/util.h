388e9f3f3f53e9c80cec2e094f3310504978699a
DehNutCase <dslayerz@hotmail.com>
2017-05-06 19:03:35 -0700

    Create util.h

---
+++ Ch 1/11.4/util.h
@@ -0,0 +1,35 @@
+#ifndef UTIL_H
+#define UTIL_H
+#include "ccc_empl.h"
+
+#include <vector>
+
+using namespace std;
+
+/**
+Swaps two integers.
+@param x the first integer to swap
+@param y the second integer to swap
+*/
+void swap(Employee& x, Employee& y);
+
+/**
+Prints all elements in a vector
+@param a the vector to print
+*/
+void print(vector<Employee> a);
+
+/**
+Sets the seed of the random number generator.
+*/
+void rand_seed();
+
+/**
+Computes a random integer in a range.
+@param a the bottom of the range
+@param b the top of the range
+@return a random integer x, a <= x and x <= b
+*/
+int rand_int(int a, int b);
+
+#endif 
